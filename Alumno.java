59aa0f3aa9e1470a54328d1408326f4b06c85674
Pablo <pabloalvgar10@gmail.com>
2021-11-24 12:03:03 +0100

    3333.01

--- Alumno.java
+++ Alumno.java
@@ -1,5 +1,5 @@
 public class Alumno {
-    
+
     // el nombre completo del alumno
     private String nombre;
     // el numero de matricula
@@ -14,6 +14,13 @@ public class Alumno {
         nombre = nombreCompleto;
         numeroMatricula = numeroMatriculaAlumno;
         edad = edadAlumno;
+
+        if (nombre.length() < 3){
+            System.out.println ("error");
+        } 
+        if(numeroMatricula.length() < 4){
+            System.out.println ("error");
+        }
     }
 
     /**
@@ -22,26 +29,35 @@ public class Alumno {
     public String getNombre() {
         return nombre;
     }
-    
+
     /**
      * Cambia el nombre del alumno
      */
     public void cambiarnombre(String nuevoNombre) {
         nombre = nuevoNombre;
     }
-    
+
     /**
      * Imprime por pantalla los detalles del alumno
      */
     public void imprimeDetalles() {
         System.out.println(nombre + " (" + numeroMatricula + ") - " + edad + " años");
     }
-    
+
     /**
      * Devuelve el nombre de usuario que el alumno debe configurar
      * en su cuenta de Github en formato de 7 caracateres
      */
+
+    public String getTrozoTexto() {
+        String texto = "Hola!";
+        return texto.substring(0, 2);
+    }
+
     public String getNombreUsuarioGithub() {
+
         return nombre.substring(0, 3) + numeroMatricula.substring(0, 4);
     }
 }
+
+
